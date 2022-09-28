47bc17a7a6ceb48cab5e2f6c5aaa156f36217237
RohanRockz <113856627+RohanRockz@users.noreply.github.com>
2022-09-29 01:12:59 +0530

    Time: 1835 ms (44.45%), Space: 294.3 MB (98.40%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -8,23 +8,22 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+//an efficient solution learnt from discussion without using third pointer.
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        ListNode* temp=new ListNode;
-        temp->next=head;
-        if(head==NULL){
-            return NULL;
-        }
-        ListNode* slow=temp;
-        ListNode* fast=head;
-        while(fast!=NULL&& fast->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
-        }
-        slow->next=slow->next->next;
+       ListNode* slow = head;
+        ListNode* fast = slow->next;
         
-        return temp->next;
+        if(fast==nullptr)
+            return nullptr;
         
+        while(fast->next!=nullptr && fast->next->next!=nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        slow->next = slow->next->next;
+        
+        return head;
     }
 };
