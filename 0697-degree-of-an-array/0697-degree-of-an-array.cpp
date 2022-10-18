afab62d50d05ab2dbd371930d60939c22dd4f2d5
Geek_Rohan <113856627+RohanRockz@users.noreply.github.com>
2022-10-18 13:39:42 +0530

    Time: 86 ms (55.47%), Space: 27.7 MB (25.95%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.cpp
+++ 0697-degree-of-an-array/0697-degree-of-an-array.cpp
@@ -1,45 +1,25 @@
-//Use of unordered map using <int,Elements> where Elements may contain various variables for that we define another class where the different variables of the class are stored.
-class Elements{
-     public:
-       int freq;
-       int li;
-       int ri;
-     Elements(){
-         freq=0;
-         li=0;
-         ri=0;
-     }
- };
-
+//Use of vectors and functions like second.back and second.front for map hashing.Best time complexity of this problem.
 class Solution {
 public:
     int findShortestSubArray(vector<int>& nums) {
-        unordered_map<int,Elements>a;
-        for(int i=0;i<nums.size();i++){
-            if(a.count(nums[i])==0){
-                a[nums[i]].freq++;
-                a[nums[i]].li=i;
-                a[nums[i]].ri=i;
-            }
-            else{
-                a[nums[i]].freq++;
-                a[nums[i]].ri=i;
-            }
+      int ans = 0;
+        int maxFrequency = INT_MIN;
+        unordered_map<int,vector<int>> m;
+        for(int i = 0; i < nums.size(); i++) {
+            m[nums[i]].push_back(i);
         }
-        int max=INT_MIN;
-        int deg=nums.size()-1;
-        for(int i=0;i<nums.size();i++){
-            if(a[nums[i]].freq>max){
-                max=a[nums[i]].freq;
-                deg=a[nums[i]].ri-a[nums[i]].li+1;
-            }
-            if(a[nums[i]].freq==max){
-                if(deg>(a[nums[i]].ri-a[nums[i]].li+1)){
-                    deg=a[nums[i]].ri-a[nums[i]].li+1;
-                }
-            }
+        for(auto& p : m) {
+           int frequency = p.second.size();
+           int len = p.second.back()-p.second.front()+1;
+           if (frequency > maxFrequency) {
+               maxFrequency = frequency;
+               ans = len;
+           }
+           else if(frequency == maxFrequency) {
+               ans = min(ans,len);
+           }
         }
-        return deg;
+        return ans;
     }
         
 };
