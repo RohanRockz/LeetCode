dec0034329123682c39626e721cabedfbdc8e7f1
Geek_Rohan <113856627+RohanRockz@users.noreply.github.com>
2022-10-21 09:28:06 +0530

    Time: 178 ms (57.25%), Space: 51.2 MB (71.05%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,14 +1,17 @@
 //Using set the problem can be solved as follows:
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int>a;
-        for(int i=0;i<nums.size();i++){
-            if(a.count(nums[i])){
+   bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> a;
+        
+        for (int num : nums) {
+            if (a.find(num) != a.end()) {
                 return true;
             }
-            a.insert(nums[i]);
+            
+            a.insert(num);
         }
+        
         return false;
     }
 };
