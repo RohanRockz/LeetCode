a304075d1dab785e727d5618185064202a70fa55
Geek_Rohan <113856627+RohanRockz@users.noreply.github.com>
2022-10-28 13:18:28 +0530

    Time: 36 ms (25.19%), Space: 13 MB (95.87%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        bool ismark=false;
+        int r=matrix.size();
+        int c=matrix[0].size();
+        for(int i=0;i<r;i++){
+            if(matrix[i][0]==0){
+                ismark=true;
+            }
+            for(int j=1;j<c;j++){
+                if(matrix[i][j]==0){
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        }
+        for(int i=1;i<r;i++){
+            for(int j=1;j<c;j++){
+                if(matrix[i][0]==0||matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(matrix[0][0]==0){
+            for(int j=0;j<c;j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(ismark){
+            for(int i=0;i<r;i++){
+                matrix[i][0]=0;
+            }
+        }
+    }
+};
