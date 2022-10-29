03f9d4fc3735091ddd4122076c1a759175287cab
Geek_Rohan <113856627+RohanRockz@users.noreply.github.com>
2022-10-29 12:54:20 +0530

    Time: 314 ms (98.39%), Space: 158.4 MB (83.13%) - LeetHub

---
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int earliestFullBloom(vector<int>& plantTime, vector<int>& growTime) {
+        int n=growTime.size();
+        vector<pair<int,int>>a(n);
+        for(int i=0;i<n;i++){
+            a[i].first=-growTime[i];
+            a[i].second=plantTime[i];
+        }
+        int total=0;
+        int cur=0;
+        sort(a.begin(),a.end());
+        for(int i=0;i<n;i++){
+            total=max(total,cur+a[i].second-a[i].first);
+            cur=cur+a[i].second;
+        }
+        return total;
+    }
+};
