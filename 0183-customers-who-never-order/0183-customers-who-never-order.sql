eb4c9a163ebf16b8dfe6cb51322161627ee9350b
Geek_Rohan <113856627+RohanRockz@users.noreply.github.com>
2022-11-12 16:29:35 +0530

    Time: 869 ms (23.88%), Space: 0B (100.00%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,5 +1,5 @@
 #Use of NOT IN Clause in the below command.
 # Write your MySQL query statement below
-Select customers.name as 'Customers'
-from customers
-where customers.id not in(select customerid from orders);
+SELECT A.Name "Customers"
+from Customers A
+WHERE A.Id NOT IN (SELECT B.CustomerId from Orders B)
