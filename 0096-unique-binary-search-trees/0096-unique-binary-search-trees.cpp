a6aff635c79f53f5072abeaaf52ba8894dde3476
Geek_Rohan <113856627+RohanRockz@users.noreply.github.com>
2022-11-21 14:32:20 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (90.72%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
+    int a[20]{};
     int numTrees(int n) {
-        if(n<=1){
-            return 1;
-        }
-        int ans=0;
+       if(n<=1){
+           return 1;
+       }
+        if(a[n])
+            return a[n];
         for(int i=1;i<=n;i++){
-            ans=ans+(numTrees(i-1)*numTrees(n-i));
+            a[n]=a[n]+(numTrees(i-1)*numTrees(n-i));
         }
-        return ans;
+        return a[n];
     }
 };
