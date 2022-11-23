4d868756bce42ab3546846b5c7cd4fc663315eab
Geek_Rohan <113856627+RohanRockz@users.noreply.github.com>
2022-11-23 23:59:00 +0530

    Time: 7 ms (45.45%), Space: 10.9 MB (59.03%) - LeetHub

---
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int dominantIndex(vector<int>& nums) {
+        int max1=0,max2=0,pos=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>max1){
+                max2=max1;
+                max1=nums[i];
+                pos=i;
+            }
+            else if(nums[i]>max2){
+                max2=nums[i];
+            }
+    }
+        return max1>=2*max2?pos:-1;
+    }
+};
