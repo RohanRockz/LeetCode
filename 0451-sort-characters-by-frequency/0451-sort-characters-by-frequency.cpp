a617033fc370172c9dc3018461282431eb13f005
Geek_Rohan <113856627+RohanRockz@users.noreply.github.com>
2023-01-05 13:53:51 +0530

    Time: 43 ms (15.67%), Space: 8 MB (93.66%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        int cnt[128]={};
+        for(char c:s){
+            cnt[c]++;
+        }
+        sort(s.begin(),s.end(),[&](char a,char b){
+            return cnt[a]>cnt[b] ||(cnt[a]==cnt[b]&& a<b);
+        });
+        return s;
+    }
+};
