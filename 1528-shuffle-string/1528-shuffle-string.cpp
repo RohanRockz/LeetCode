a40b017c0843b9657ec9d756f3426daa1dfab209
Geek_Rohan <113856627+RohanRockz@users.noreply.github.com>
2023-01-26 00:39:56 +0530

    Time: 15 ms (36.69%), Space: 15.3 MB (48.20%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+         int i=0;
+        while(i!=indices.size()){
+            if(i==indices[i]){
+                i++;
+            }
+            else{
+                swap(s[i],s[indices[i]]);
+                swap(indices[i],indices[indices[i]]);
+            }
+        }
+        return s;
+    }
+};
