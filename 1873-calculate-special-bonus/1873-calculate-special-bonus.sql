9bce447ba678b1a3603858f5446a3fd543e4ceff
Geek_Rohan <113856627+RohanRockz@users.noreply.github.com>
2023-01-29 01:36:14 +0530

    Time: 1130 ms (28.06%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
- select employee_id , salary * ( employee_id%2 ) * ( name not like 'M%') as bonus
-    from employees
-    order by employee_id;
+select employee_id,salary*(employee_id%2)*(name not like 'M%') as bonus
+from employees 
+order by employee_id;
