fa66266074673b4487353915560c73310b74283e
Geek_Rohan <113856627+RohanRockz@users.noreply.github.com>
2023-03-03 15:21:39 +0530

    Time: 3 ms (50.73%), Space: 6 MB (68.26%) - LeetHub

---
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
@@ -0,0 +1,17 @@
+//Excellent approach of solving the problem using permutation concept!!.
+class Solution {
+public:
+    
+    int countNumbersWithUniqueDigits(int n) {
+        if(n==0){
+            return 1;
+        }
+        int ans=10,calc=9,temp=9;
+        for(int i=0;i<n-1;i++){
+            calc*=temp;
+            ans=ans+calc;
+            temp--;
+        }
+        return ans;
+    }
+};
