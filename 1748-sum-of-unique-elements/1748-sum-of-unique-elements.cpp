5a50b842d50ef4097304ccf5924e84764df2975d
Geek_Rohan <113856627+RohanRockz@users.noreply.github.com>
2023-03-06 19:40:09 +0530

    Time: 3 ms (64.42%), Space: 8.1 MB (30.88%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        int sum=0;
+        for(auto i:mp){
+            if(i.second==1){
+                sum+=i.first;
+            }
+        }
+        return sum;
+    }
+};
