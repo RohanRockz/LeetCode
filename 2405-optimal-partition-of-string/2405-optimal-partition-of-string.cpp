b3a0391e07fa99d05fae01afc5efd206ef35706f
Geek_Rohan <113856627+RohanRockz@users.noreply.github.com>
2023-04-04 14:13:50 +0530

    Time: 90 ms (69.69%), Space: 53.3 MB (32.70%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -3,15 +3,15 @@ class Solution {
 public:
     int partitionString(string s) {
         int res=1;
-        set<char>a;
+        vector<int> a(26, 0);
         for(int i=0;i<s.size();i++){
-            if(a.find(s[i])==a.end()){
-                a.insert(s[i]);
+            if(!a[s[i] - 'a']){
+                a[s[i] - 'a']++;
             }
             else{
                 res++;
-                a.clear();//function used to clear the set 
-                a.insert(s[i]);
+                a = vector<int> (26, 0);//function used to clear the set 
+                a[s[i] - 'a']++;
             }
         }
         return res;
