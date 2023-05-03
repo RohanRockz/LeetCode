cc26b2a6078eec628cbdff4a72f080a7f8cb8393
Geek_Rohan <113856627+RohanRockz@users.noreply.github.com>
2023-05-03 23:27:31 +0530

    Time: 36 ms (95.56%), Space: 26.3 MB (98.19%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        vector<vector<int>>answer(2);
-        int p=0,q=0;
-        for(int i=0;i<nums1.size();i++){
-            if(find(nums2.begin(),nums2.end(),nums1[i])==nums2.end()&&find(answer[0].begin(),answer[0].end(),nums1[i])==answer[0].end()){
-                answer[0].push_back(nums1[i]);
-            }
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) 
+    {
+        int m[2001] = {0};
+        
+        for(int i : nums1)
+            m[i + 1000] = 1;
+        
+        vector<vector<int>> a(2);
+        
+        for(int i : nums2)
+        {
+            if(!m[i + 1000])
+                m[i + 1000] = -1, a[1].push_back(i);
+            else if(m[i + 1000] == 1)
+                m[i + 1000] *= -1;
         }
-        for(int i=0;i<nums2.size();i++){
-            if(find(nums1.begin(),nums1.end(),nums2[i])==nums1.end()&&find(answer[1].begin(),answer[1].end(),nums2[i])==answer[1].end()){
-                answer[1].push_back(nums2[i]);
-            }
+        
+        for(int i = 0; i < 2001; i++)
+        {
+            if(m[i] == 1)
+                a[0].push_back(i - 1000);
         }
-        return answer;
+        
+        return a;
     }
 };
