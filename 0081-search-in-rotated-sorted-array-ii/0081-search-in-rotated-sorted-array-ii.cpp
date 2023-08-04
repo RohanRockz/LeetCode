6e3ffce82c08c1ac79f58690cae0f6ffa512c932
Geek_Rohan <113856627+RohanRockz@users.noreply.github.com>
2023-08-04 23:50:14 +0530

    Time: 3 ms (92.42%), Space: 13.9 MB (48.47%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n=nums.size();
+        if(n==0)
+            return false;
+        int end=n-1;
+        int start=0;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                return true;
+            }
+            if(!isBinarySearchHelpful(nums,start,nums[mid])){
+                start++;
+                continue;
+            }
+            bool pivotArray=existsInFirst(nums,start,nums[mid]);
+            bool targetArray=existsInFirst(nums,start,target);
+            if(pivotArray^targetArray){
+                if(pivotArray){
+                    start=mid+1;
+                }
+                else{
+                    end=mid-1;
+                }
+            }else{
+                if(nums[mid]<target){
+                    start=mid+1;
+                }else{
+                    end=mid-1;
+                }
+            }
+            
+        }
+        return false;
+    }
+      bool isBinarySearchHelpful(vector<int>& nums, int start, int element) {
+        return nums[start] != element;
+    }
+
+    // returns true if element exists in first array, false if it exists in second
+    bool existsInFirst(vector<int>& nums, int start, int element) {
+        return nums[start] <= element;
+    }
+};
