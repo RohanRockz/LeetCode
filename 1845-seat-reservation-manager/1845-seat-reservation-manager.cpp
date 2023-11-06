546101f4738161f1a7467ac1f2a05ada0163af32
Geek_Rohan <113856627+RohanRockz@users.noreply.github.com>
2023-11-07 00:45:42 +0530

    Time: 306 ms (58.54%), Space: 148.3 MB (33.43%) - LeetHub

---
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.cpp
@@ -0,0 +1,31 @@
+class SeatManager {
+    // Min heap to store all unreserved seats.
+    priority_queue<int, vector<int>, greater<int>> availableSeats;
+
+public:
+    SeatManager(int n) {
+        // Initially all seats are unreserved.
+        for (int seatNumber = 1; seatNumber <= n; ++seatNumber) {
+            availableSeats.push(seatNumber);
+        }
+    }
+
+    int reserve() {
+        // Get the smallest-numbered unreserved seat from the min heap.
+        int seatNumber = availableSeats.top();
+        availableSeats.pop();
+        return seatNumber;
+    }
+
+    void unreserve(int seatNumber) {
+        // Push the unreserved seat in the min heap.
+        availableSeats.push(seatNumber);
+    }
+};
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager* obj = new SeatManager(n);
+ * int param_1 = obj->reserve();
+ * obj->unreserve(seatNumber);
+ */
