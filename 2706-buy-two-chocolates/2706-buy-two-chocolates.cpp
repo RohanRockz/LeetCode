398903ed031b98a6e1c39908ea483f9e21681767
Geek_Rohan <113856627+RohanRockz@users.noreply.github.com>
2023-12-21 00:25:12 +0530

    Time: 7 ms (91.14%), Space: 46.9 MB (58.30%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(),prices.end());
+        int minCost=prices[0]+prices[1];
+        if(minCost<=money){
+            return money-minCost;
+        }
+        else{
+            return money;
+        }
+    }
+};
