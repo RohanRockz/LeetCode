46b34bae68739c379b42be2a204374f4ea4472c7
Geek_Rohan <113856627+RohanRockz@users.noreply.github.com>
2024-09-12 22:39:46 +0530

    Time: 55 ms (24.15%), Space: 33.9 MB (58.62%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
-        int res=words.size();
-        bool alpha[26]={};
-        for(int i=0;i<allowed.size();i++){
-            alpha[allowed[i]-'a']=true;
-        }
-        for(string word:words){
-            for(char c:word){
-                if(!alpha[c-'a']){
-                    res--;
-                    break;
+        int consistentCount = 0;
+
+        // Iterate through each word in the words array
+        for (string& word : words) {
+            bool isWordConsistent = true;
+
+            // Check each character in the current word
+            for (int i = 0; i < word.length(); i++) {
+                char currentChar = word[i];
+                bool isCharAllowed = false;
+
+                // Check if the current character is in the allowed string
+                for (int j = 0; j < allowed.length(); j++) {
+                    if (allowed[j] == currentChar) {
+                        isCharAllowed = true;
+                        break;  // Character found, no need to continue
+                                // searching
+                    }
+                }
+
+                // If the character is not allowed, mark the word as
+                // inconsistent
+                if (!isCharAllowed) {
+                    isWordConsistent = false;
+                    break;  // No need to check remaining characters
                 }
             }
+
+            // If the word is consistent, increment the count
+            if (isWordConsistent) {
+                consistentCount++;
+            }
         }
-        return res;
+
+        return consistentCount;
     }
 };
